f7f2d5a3261c2052b87152e7b080cb7b6e7bb71c
Echo4468 <321411510@qq.com>
2023-12-31 19:06:16 +0800

    修改宽度

--- src/views/product/Attr/index.vue
+++ src/views/product/Attr/index.vue
@@ -31,19 +31,19 @@
      <div v-show="!isShowTable">
       <el-form :inline="true" ref="form">
         <el-form-item label="属性名">
-            <el-input placeholder="请输入属性名"></el-input>
+            <el-input placeholder="请输入属性名" label-width="80px"></el-input>
         </el-form-item>
       </el-form>
-      <el-button icon="el-icon-plus" type="primary" style="margin:0px 0px 10px" :disabled="true" @click="isShowTable=false">添加属性值</el-button>
+      <el-button icon="el-icon-plus" type="primary"  :disabled="true" @click="isShowTable=false">添加属性值</el-button>
        <el-button>取消</el-button>
-       <el-table :data="attrList" style="{width: 100%; margin: 10px 0px}" border>
+       <el-table :data="attrList" style="width: 100%; margin: 20px 0px" border>
         <el-table-column type="index" label="序号" width="80" align="center"></el-table-column>
         <el-table-column prop="attrName" label="属性值名称" width="250" align="center"></el-table-column>
         <el-table-column prop="address" label="操作" align="center">
         </el-table-column>
       </el-table>
       <el-button type="primary">保存</el-button>
-      <el-button  @click="isShowTable="true">取消</el-button>
+      <el-button  @click="isShowTable=true">取消</el-button>
      </div>
     </el-card>
   </div>
